104d3677dbbaca1467e93936c8982c008ba2eb7f
kelsey Brown <kelsey.brown@kellogg.northwestern.edu>
2025-03-13 02:18:45 +0000

    code along

--- tacostagram.js
+++ tacostagram.js
@@ -1,6 +1,6 @@
 function getPostsFromAPI() {
   // Replace this with the URL of the JSON API that returns an array of image URLs
-  const url = 'YOUR_URL_GOES_HERE';
+  const url = 'https://3000-kelseybrown-tacogramfin-elj2ds7udh4.ws-us118.gitpod.io/posts.json';
   if (url == 'YOUR_URL_GOES_HERE') {
     alert('Error: Replace url value in tacostagram.js')
   }
@@ -8,15 +8,19 @@ function getPostsFromAPI() {
   // Make a GET request to the API
   fetch(url)
     .then(response => response.json()) // Parse the response as JSON
-    .then(posts => {
+    .then(posts => 
       // Loop through the array of posts and build html for each
       for (let post of posts) {
         // Log post data to browser console
         console.log(post);
 
         // html for each post goes here
-        let html = ``;
-
+        let html = '
+        <div class="col-sm-6 col-md-3">
+          <img src="${post["image"]}class="img-fluid">
+          <p>${post["body"]}</p>
+        </div>
+          ';
         const postsDiv = document.querySelector('#posts');
         postsDiv.innerHTML += html
       };
